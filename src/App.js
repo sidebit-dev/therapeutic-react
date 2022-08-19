8fdfb6b21820c9dc2c8fc10d7100cab5db77aa6c
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-08-18 22:17:04 -0300

    Cria componente Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './components/header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello word</h1>
+    <div className='w3-top'>
+    <Header />
     </div>
   );
 }
